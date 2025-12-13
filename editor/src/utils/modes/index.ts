a242d86194232b7926cbe9954c84cee5b3dd740b
MIT2014 <flow@livemail.tw>
2025-12-13 13:34:31 +0800

    refactor, reorder

--- editor/src/utils/modes/index.ts
+++ editor/src/utils/modes/index.ts
@@ -2,8 +2,8 @@
 export type { ModeStrategy, ModeContext } from './ModeStrategy';
 export { ModeManager } from './ModeManager';
 export { ViewMode } from './ViewMode';
-export { DelRectMode } from './DelRectMode';
 export { DrawPolylineMode } from './DrawPolylineMode';
 export { DrawScribbleMode } from './DrawScribbleMode';
+export { DelRectMode } from './DelRectMode';
 export { DelBlockMode } from './DelBlockMode';
 export { DelPixelMode } from './DelPixelMode';
